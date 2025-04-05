fb9c01c1f740d05c12c7524991f09b844d25a1e1
Exetria <107202768+Exetria@users.noreply.github.com>
2025-04-06 00:10:23 +0700

    Create Dashboard Page for Windows App

---
+++ apps/windows_app/lib/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:windows_app/customer_module/pages/customerListFragment.dart';
+import 'package:windows_app/order_module/pages/orderListFragment.dart';
+import 'package:windows_app/product_module/pages/productListFragment.dart';
+import 'package:windows_app/report_module/mainReportFragment.dart';
+import 'package:windows_app/sales_module/pages/salesListFragment.dart';
+import 'package:windows_app/user_management_module/pages/profileFragment.dart';
+import 'package:windows_app/variables.dart';
+
+final selectedIndexProvider = StateProvider<int>((ref) => 0);
+
+class HomePage extends StatefulHookConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  ConsumerState<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends ConsumerState<HomePage> {
+  final List<Widget> pages = [
+    const MainReportFragment(),
+    const SalesListFragment(),
+    const CustomerListFragment(),
+    const ProductListFragment(),
+    const OrderListFragment(),
+    const ProfileFragment(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedIndex = ref.watch(selectedIndexProvider);
+
+    return Scaffold(
+      body: Row(
+        children: [
+          // SIDE NAVBAR
+          Container(
+            constraints: const BoxConstraints(maxWidth: 100),
+            decoration: BoxDecoration(
+              border: Border(
+                right: BorderSide(color: dividerColor, width: 0.7),
+              ),
+            ),
+            child: NavigationRail(
+              selectedIndex: selectedIndex,
+              onDestinationSelected:
+                  (index) =>
+                      ref.read(selectedIndexProvider.notifier).state = index,
+              labelType: NavigationRailLabelType.all,
+              leading: Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Center(
+                  child: Image.asset('assets/logo.png', height: 70.h),
+                ),
+              ),
+              destinations: [
+                const NavigationRailDestination(
+                  icon: Icon(Icons.dashboard),
+                  label: Text('Home'),
+                ),
+                const NavigationRailDestination(
+                  icon: Icon(Icons.people),
+                  label: Text('Sales'),
+                ),
+                const NavigationRailDestination(
+                  icon: Icon(Icons.business),
+                  label: Text('Customer'),
+                ),
+                const NavigationRailDestination(
+                  icon: Icon(Icons.inventory_2),
+                  label: Text('Product'),
+                ),
+                const NavigationRailDestination(
+                  icon: Icon(Icons.receipt_long),
+                  label: Text('Order'),
+                ),
+                const NavigationRailDestination(
+                  icon: Icon(Icons.person),
+                  label: Text('Profile'),
+                ),
+              ],
+            ),
+          ),
+
+          // CONTENT
+          Expanded(child: pages[selectedIndex]),
+        ],
+      ),
+    );
+  }
+}
--- apps/windows_app/lib/main.dart
+++ apps/windows_app/lib/main.dart
@@ -1,23 +1,83 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:windows_app/firebase_options.dart';
+import 'package:windows_app/home_page.dart';
+import 'package:windows_app/variables.dart';
+
+final lightTheme = ThemeData(
+  // light/dark mode
+  brightness: Brightness.light,
+
+  // default scaffold bg color
+  scaffoldBackgroundColor: backgroundColor,
+
+  // main color (buttons, sliders, switches, etc.)
+  primaryColor: primaryColor,
+
+  // main app color scheme
+  colorScheme: ColorScheme.light(
+    primary: primaryColor,
+    secondary: secondaryColor,
+    surface: backgroundColor,
+    onPrimary: backgroundColor,
+    onSurface: textColor,
+  ),
+
+  // main app icon scheme
+  iconTheme: IconThemeData(color: textColor),
+
+  // navrail theme
+  navigationRailTheme: NavigationRailThemeData(
+    backgroundColor: backgroundColor,
+    selectedIconTheme: IconThemeData(color: selectedItemColor),
+    unselectedIconTheme: IconThemeData(color: unselectedItemColor),
+    selectedLabelTextStyle: subtitleStyle.copyWith(color: selectedItemColor),
+    unselectedLabelTextStyle: subtitleStyle.copyWith(
+      color: unselectedItemColor,
+    ),
+    indicatorColor: tertiaryColor,
+  ),
+
+  // card color & theme
+  cardColor: backgroundColor,
+  cardTheme: CardTheme(
+    color: backgroundColor,
+    elevation: 4,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+  ),
+  textTheme: GoogleFonts.montserratTextTheme(),
+);
 
 void main() async {
-  // TODO Setting system
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  runApp(const Main());
+  runApp(const ProviderScope(child: Main()));
 }
 
-class Main extends StatelessWidget {
+class Main extends StatefulHookConsumerWidget {
   const Main({super.key});
 
   @override
+  ConsumerState<Main> createState() => _MainApp();
+}
+
+class _MainApp extends ConsumerState<Main> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return ScreenUtilInit(
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: lightTheme,
+          home: child,
+        );
+      },
+      child: const SafeArea(child: HomePage()),
     );
   }
 }
