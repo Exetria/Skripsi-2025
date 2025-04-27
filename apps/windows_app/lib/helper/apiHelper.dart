f9bbb40e7431ee388af6d30c097a1a0973e8c697
Exetria <107202768+Exetria@users.noreply.github.com>
2025-04-27 17:44:37 +0700

    Create Windows API Helpers

--- apps/windows_app/lib/helper/apiHelper.dart
+++ apps/windows_app/lib/helper/apiHelper.dart
@@ -1,188 +1,184 @@
 import 'dart:convert';
 
+import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
 import 'package:windows_app/helper/userDataHelper.dart';
 
-class ApiResponse<T> {
-  final T? data;
-  final String? error;
+class ApiException implements Exception {
+  final int? statusCode;
+  final String? message;
+  final Map<String, dynamic>? responseBody;
 
-  ApiResponse.success(this.data) : error = null;
-  ApiResponse.failure(this.error) : data = null;
+  ApiException({this.statusCode, this.message, this.responseBody});
 
-  bool get isSuccess => data != null;
+  @override
+  String toString() {
+    return 'ApiException(statusCode: $statusCode, responseBody: $responseBody)';
+  }
+}
 
-  R fold<R>(R Function(T data) onSuccess, R Function(String error) onFailure) {
-    if (isSuccess) {
-      return onSuccess(data as T);
+// safety function for call remote process
+Future<Either<ApiException, T>> remoteProcess<T>(Future<T> process) async {
+  try {
+    var processResult = await process;
+    return Right(processResult);
+  } catch (e) {
+    if (e is ApiException) {
+      return Left(e);
+    } else if (e is Exception) {
+      return Left(ApiException(message: e.toString()));
     } else {
-      return onFailure(error ?? 'Unknown error');
+      return Left(ApiException(message: 'Unknown error'));
     }
   }
 }
 
 // GET request (no body)
-Future<ApiResponse<Map<String, dynamic>>> apiCallGet(
+Future<Map<String, dynamic>> apiCallGet(
   String url, {
   bool useHeader = true,
 }) async {
   final uri = Uri.parse(url);
 
-  try {
-    Map<String, String> _headers =
-        useHeader
-            ? {
-              'Authorization': 'Bearer ${userDataHelper.idToken}',
-              'Content-Type': 'application/json',
-            }
-            : {};
+  Map<String, String> _headers =
+      useHeader
+          ? {
+            'Authorization': 'Bearer ${userDataHelper.idToken}',
+            'Content-Type': 'application/json',
+          }
+          : {};
 
-    final response = await http.get(uri, headers: _headers);
+  final response = await http.get(uri, headers: _headers);
 
-    if (response.statusCode == 200) {
-      return ApiResponse.success(json.decode(response.body));
-    } else {
-      return ApiResponse.failure(
-        'Error ${response.statusCode}: ${response.body}',
-      );
-    }
-  } catch (e) {
-    return ApiResponse.failure('Exception: ${e.toString()}');
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      responseBody: json.decode(response.body),
+    );
   }
 }
 
 // POST request (with optional body)
-Future<ApiResponse<Map<String, dynamic>>> apiCallPost(
+Future<Map<String, dynamic>> apiCallPost(
   String url, {
   Map<String, dynamic>? body,
   bool useHeader = true,
 }) async {
   final uri = Uri.parse(url);
 
-  try {
-    Map<String, String> _headers =
-        useHeader
-            ? {
-              'Authorization': 'Bearer ${userDataHelper.idToken}',
-              'Content-Type': 'application/json',
-            }
-            : {};
+  Map<String, String> _headers =
+      useHeader
+          ? {
+            'Authorization': 'Bearer ${userDataHelper.idToken}',
+            'Content-Type': 'application/json',
+          }
+          : {};
 
-    final response = await http.post(
-      uri,
-      headers: _headers,
-      body: jsonEncode(body ?? {}),
+  final response = await http.post(
+    uri,
+    headers: _headers,
+    body: jsonEncode(body ?? {}),
+  );
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      responseBody: json.decode(response.body),
     );
-
-    if (response.statusCode == 200) {
-      return ApiResponse.success(json.decode(response.body));
-    } else {
-      print('asds ${json.decode(response.body)}');
-      return ApiResponse.failure(
-        'Error ${response.statusCode}: ${response.body}',
-      );
-    }
-  } catch (e) {
-    return ApiResponse.failure('Exception: ${e.toString()}');
   }
 }
 
 // PATCH request (partial update)
-Future<ApiResponse<Map<String, dynamic>>> apiCallPatch(
+Future<Map<String, dynamic>> apiCallPatch(
   String url, {
   Map<String, dynamic>? body,
   bool useHeader = true,
 }) async {
   final uri = Uri.parse(url);
 
-  try {
-    Map<String, String> _headers =
-        useHeader
-            ? {
-              'Authorization': 'Bearer ${userDataHelper.idToken}',
-              'Content-Type': 'application/json',
-            }
-            : {};
+  Map<String, String> _headers =
+      useHeader
+          ? {
+            'Authorization': 'Bearer ${userDataHelper.idToken}',
+            'Content-Type': 'application/json',
+          }
+          : {};
 
-    final response = await http.patch(
-      uri,
-      headers: _headers,
-      body: jsonEncode(body ?? {}),
+  final response = await http.patch(
+    uri,
+    headers: _headers,
+    body: jsonEncode(body ?? {}),
+  );
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      responseBody: json.decode(response.body),
     );
-
-    if (response.statusCode == 200) {
-      return ApiResponse.success(json.decode(response.body));
-    } else {
-      return ApiResponse.failure(
-        'Error ${response.statusCode}: ${response.body}',
-      );
-    }
-  } catch (e) {
-    return ApiResponse.failure('Exception: ${e.toString()}');
   }
 }
 
 // PUT request (full update or replace)
-Future<ApiResponse<Map<String, dynamic>>> apiCallPut(
+Future<Map<String, dynamic>> apiCallPut(
   String url, {
   Map<String, dynamic>? body,
   bool useHeader = true,
 }) async {
   final uri = Uri.parse(url);
 
-  try {
-    Map<String, String> _headers =
-        useHeader
-            ? {
-              'Authorization': 'Bearer ${userDataHelper.idToken}',
-              'Content-Type': 'application/json',
-            }
-            : {};
+  Map<String, String> _headers =
+      useHeader
+          ? {
+            'Authorization': 'Bearer ${userDataHelper.idToken}',
+            'Content-Type': 'application/json',
+          }
+          : {};
 
-    final response = await http.put(
-      uri,
-      headers: _headers,
-      body: jsonEncode(body ?? {}),
+  final response = await http.put(
+    uri,
+    headers: _headers,
+    body: jsonEncode(body ?? {}),
+  );
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      responseBody: json.decode(response.body),
     );
-
-    if (response.statusCode == 200) {
-      return ApiResponse.success(json.decode(response.body));
-    } else {
-      return ApiResponse.failure(
-        'Error ${response.statusCode}: ${response.body}',
-      );
-    }
-  } catch (e) {
-    return ApiResponse.failure('Exception: ${e.toString()}');
   }
 }
 
 // DELETE request
-Future<ApiResponse<Map<String, dynamic>>> apiCallDelete(
+Future<Map<String, dynamic>> apiCallDelete(
   String url, {
   bool useHeader = true,
 }) async {
   final uri = Uri.parse(url);
 
-  try {
-    Map<String, String> _headers =
-        useHeader
-            ? {
-              'Authorization': 'Bearer ${userDataHelper.idToken}',
-              'Content-Type': 'application/json',
-            }
-            : {};
+  Map<String, String> _headers =
+      useHeader
+          ? {
+            'Authorization': 'Bearer ${userDataHelper.idToken}',
+            'Content-Type': 'application/json',
+          }
+          : {};
 
-    final response = await http.delete(uri, headers: _headers);
+  final response = await http.delete(uri, headers: _headers);
 
-    if (response.statusCode == 200) {
-      return ApiResponse.success(json.decode(response.body));
-    } else {
-      return ApiResponse.failure(
-        'Error ${response.statusCode}: ${response.body}',
-      );
-    }
-  } catch (e) {
-    return ApiResponse.failure('Exception: ${e.toString()}');
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      responseBody: json.decode(response.body),
+    );
   }
 }
