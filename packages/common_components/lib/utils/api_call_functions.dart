affabde32ac7c4c4121269d6eaff6bb79e6690d8
Exetria <107202768+Exetria@users.noreply.github.com>
2025-04-30 20:10:32 +0700

    Package - Update API Call Functions

--- packages/common_components/lib/utils/api_call_functions.dart
+++ packages/common_components/lib/utils/api_call_functions.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:common_components/global/user_data_helper.dart';
 import 'package:common_components/utils/api_exception.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:http/http.dart' as http;
@@ -11,198 +10,130 @@ Future<Either<ApiException, T>> remoteProcess<T>(Future<T> process) async {
   try {
     var processResult = await process;
     return Right(processResult);
+  } on SocketException {
+    return Left(ApiException(statusCode: 0, message: "Connection Error"));
   } catch (e) {
     if (e is ApiException) {
       return Left(e);
     } else if (e is Exception) {
-      return Left(ApiException(statusCode: -2, message: e.toString()));
+      return Left(ApiException(statusCode: -1, message: e.toString()));
     } else {
-      return Left(ApiException(statusCode: -2, message: 'Unknown error'));
+      return Left(ApiException(statusCode: -1, message: 'Unknown error'));
     }
   }
 }
 
 // GET request (no body)
-Future<Map<String, dynamic>> apiCallGet(
-  String url, {
-  bool useHeader = true,
+Future<Map<String, dynamic>> apiCallGet({
+  required String url,
+  required Map<String, String> headers,
 }) async {
   final uri = Uri.parse(url);
 
-  Map<String, String> headers =
-      useHeader
-          ? {
-            'Authorization': 'Bearer ${userDataHelper.idToken}',
-            'Content-Type': 'application/json',
-          }
-          : {};
+  final response = await http.get(uri, headers: headers);
 
-  try {
-    final response = await http.get(uri, headers: headers);
-
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw ApiException(
-        statusCode: response.statusCode,
-        message: "success",
-        responseBody: json.decode(response.body),
-      );
-    }
-  } on SocketException {
-    throw ApiException(statusCode: 0, message: "Connection Error");
-  } catch (e) {
-    throw ApiException(statusCode: -1, message: "Unknown Error");
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      message: "success",
+      responseBody: json.decode(response.body),
+    );
   }
 }
 
 // POST request (with optional body)
-Future<Map<String, dynamic>> apiCallPost(
-  String url, {
+Future<Map<String, dynamic>> apiCallPost({
+  required String url,
+  required Map<String, String> headers,
   Map<String, dynamic>? body,
-  bool useHeader = true,
 }) async {
   final uri = Uri.parse(url);
 
-  Map<String, String> headers =
-      useHeader
-          ? {
-            'Authorization': 'Bearer ${userDataHelper.idToken}',
-            'Content-Type': 'application/json',
-          }
-          : {};
+  final response = await http.post(
+    uri,
+    headers: headers,
+    body: jsonEncode(body ?? {}),
+  );
 
-  try {
-    final response = await http.post(
-      uri,
-      headers: headers,
-      body: jsonEncode(body ?? {}),
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      message: "success",
+      responseBody: json.decode(response.body),
     );
-
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw ApiException(
-        statusCode: response.statusCode,
-        message: "success",
-        responseBody: json.decode(response.body),
-      );
-    }
-  } on SocketException {
-    throw ApiException(statusCode: 0, message: "Connection Error");
-  } catch (e) {
-    throw ApiException(statusCode: -1, message: "Unknown Error");
   }
 }
 
 // PATCH request (partial update)
-Future<Map<String, dynamic>> apiCallPatch(
-  String url, {
+Future<Map<String, dynamic>> apiCallPatch({
+  required String url,
+  required Map<String, String> headers,
   Map<String, dynamic>? body,
-  bool useHeader = true,
 }) async {
   final uri = Uri.parse(url);
 
-  Map<String, String> headers =
-      useHeader
-          ? {
-            'Authorization': 'Bearer ${userDataHelper.idToken}',
-            'Content-Type': 'application/json',
-          }
-          : {};
+  final response = await http.patch(
+    uri,
+    headers: headers,
+    body: jsonEncode(body ?? {}),
+  );
 
-  try {
-    final response = await http.patch(
-      uri,
-      headers: headers,
-      body: jsonEncode(body ?? {}),
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      message: "success",
+      responseBody: json.decode(response.body),
     );
-
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw ApiException(
-        statusCode: response.statusCode,
-        message: "success",
-        responseBody: json.decode(response.body),
-      );
-    }
-  } on SocketException {
-    throw ApiException(statusCode: 0, message: "Connection Error");
-  } catch (e) {
-    throw ApiException(statusCode: -1, message: "Unknown Error");
   }
 }
 
 // PUT request (full update or replace)
-Future<Map<String, dynamic>> apiCallPut(
-  String url, {
+Future<Map<String, dynamic>> apiCallPut({
+  required String url,
+  required Map<String, String> headers,
   Map<String, dynamic>? body,
-  bool useHeader = true,
 }) async {
   final uri = Uri.parse(url);
 
-  Map<String, String> headers =
-      useHeader
-          ? {
-            'Authorization': 'Bearer ${userDataHelper.idToken}',
-            'Content-Type': 'application/json',
-          }
-          : {};
+  final response = await http.put(
+    uri,
+    headers: headers,
+    body: jsonEncode(body ?? {}),
+  );
 
-  try {
-    final response = await http.put(
-      uri,
-      headers: headers,
-      body: jsonEncode(body ?? {}),
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      message: "success",
+      responseBody: json.decode(response.body),
     );
-
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw ApiException(
-        statusCode: response.statusCode,
-        message: "success",
-        responseBody: json.decode(response.body),
-      );
-    }
-  } on SocketException {
-    throw ApiException(statusCode: 0, message: "Connection Error");
-  } catch (e) {
-    throw ApiException(statusCode: -1, message: "Unknown Error");
   }
 }
 
 // DELETE request
-Future<Map<String, dynamic>> apiCallDelete(
-  String url, {
-  bool useHeader = true,
+Future<Map<String, dynamic>> apiCallDelete({
+  required String url,
+  required Map<String, String> headers,
 }) async {
   final uri = Uri.parse(url);
 
-  Map<String, String> headers =
-      useHeader
-          ? {
-            'Authorization': 'Bearer ${userDataHelper.idToken}',
-            'Content-Type': 'application/json',
-          }
-          : {};
+  final response = await http.delete(uri, headers: headers);
 
-  try {
-    final response = await http.delete(uri, headers: headers);
-
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw ApiException(
-        statusCode: response.statusCode,
-        message: "success",
-        responseBody: json.decode(response.body),
-      );
-    }
-  } on SocketException {
-    throw ApiException(statusCode: 0, message: "Connection Error");
-  } catch (e) {
-    throw ApiException(statusCode: -1, message: "Unknown Error");
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw ApiException(
+      statusCode: response.statusCode,
+      message: "success",
+      responseBody: json.decode(response.body),
+    );
   }
 }
