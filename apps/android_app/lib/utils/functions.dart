bd640cd232600a76cf04500566ecb070ed295ab7
Exetria <107202768+Exetria@users.noreply.github.com>
2025-04-30 20:11:56 +0700

    Android & Windows - Remove showFeedbackDialog

--- apps/android_app/lib/utils/functions.dart
+++ apps/android_app/lib/utils/functions.dart
@@ -175,56 +175,6 @@ InkWell customListItem({
   );
 }
 
-// DIALOG
-void showFeedbackDialog({
-  required BuildContext context,
-  required int type,
-  required String message,
-  Duration duration = const Duration(seconds: 1),
-  VoidCallback? onClose,
-}) {
-  showDialog(
-    context: context,
-    barrierDismissible: false,
-    builder: (context) {
-      // Start a delayed pop when the dialog builds
-      Future.delayed(duration, () {
-        if (Navigator.of(context).canPop()) {
-          Navigator.of(context).pop();
-
-          if (onClose != null) {
-            onClose();
-          }
-        }
-      });
-
-      return Dialog(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        backgroundColor: backgroundColor,
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(
-                type == 1
-                    ? Icons.check_circle_outline
-                    : type == 2
-                    ? Icons.error_outline
-                    : Icons.highlight_off,
-                size: 48,
-                color: primaryColor,
-              ),
-              const SizedBox(height: 16),
-              Text(message, style: bodyStyle, textAlign: TextAlign.center),
-            ],
-          ),
-        ),
-      );
-    },
-  );
-}
-
 // FORM DIALOG
 void showFormDialog({
   required BuildContext context,
