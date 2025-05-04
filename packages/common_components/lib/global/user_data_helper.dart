4f028d8826c4837e4cd4a66d721dd684c4c7f20f
Exetria <107202768+Exetria@users.noreply.github.com>
2025-05-04 20:11:01 +0700

    Package - Update User Data Params

--- packages/common_components/lib/global/user_data_helper.dart
+++ packages/common_components/lib/global/user_data_helper.dart
@@ -1,8 +1,10 @@
 class UserDataHelper {
   final String uid;
-  final String name;
+  final String userName;
+  final String fullName;
   final String email;
   final String phone;
+  final String photoUrl;
   final String role;
   String idToken;
   String refreshToken;
@@ -11,9 +13,11 @@ class UserDataHelper {
 
   UserDataHelper({
     required this.uid,
-    required this.name,
+    required this.userName,
+    required this.fullName,
     required this.email,
     required this.phone,
+    required this.photoUrl,
     required this.role,
     required this.idToken,
     required this.refreshToken,
