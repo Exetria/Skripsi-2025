20b059d0e81874262bec4fb54fb2bb15590e3373
Exetria <107202768+Exetria@users.noreply.github.com>
2025-05-26 21:48:34 +0700

    Package - Add buildInputBox Function

--- packages/common_components/lib/widgets.dart
+++ packages/common_components/lib/widgets.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Widget customSearchBar({
@@ -25,6 +26,34 @@ Widget customSearchBar({
         ),
       ),
       onChanged: onChanged,
+    ),
+  );
+}
+
+// INPUT ROW
+TextFormField buildInputBox({
+  required TextEditingController controller,
+  required String label,
+  List<TextInputFormatter>? inputFormatters,
+  String? Function(String?)? validator,
+  TextInputType? keyboardType,
+  String? prefix,
+  String? suffix,
+  int? maxLines = 1,
+  bool enabled = true,
+}) {
+  return TextFormField(
+    enabled: enabled,
+    controller: controller,
+    inputFormatters: inputFormatters,
+    maxLines: maxLines,
+    keyboardType: keyboardType,
+    validator: validator,
+    decoration: InputDecoration(
+      labelText: label,
+      prefixText: prefix,
+      suffixText: suffix,
+      alignLabelWithHint: true,
     ),
   );
 }
