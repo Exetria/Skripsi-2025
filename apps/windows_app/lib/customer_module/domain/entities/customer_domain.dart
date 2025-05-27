43f6f8bb0a24d78960afd1196ea5bf5538268c88
Exetria <107202768+Exetria@users.noreply.github.com>
2025-05-27 12:19:53 +0700

    Windows - Update Customer Domain

--- apps/windows_app/lib/customer_module/domain/entities/customer_domain.dart
+++ apps/windows_app/lib/customer_module/domain/entities/customer_domain.dart
@@ -19,26 +19,30 @@ class CustomerDomain with _$CustomerDomain {
 @freezed
 class Fields with _$Fields {
   const factory Fields({
+    @JsonKey(name: 'pic_national_id_photo') ApprovedBy? picNationalIdPhoto,
     @JsonKey(name: 'company_store_condition') ApprovedBy? companyStoreCondition,
-    @JsonKey(name: 'owner_name') ApprovedBy? ownerName,
-    @JsonKey(name: 'subscription_type') ApprovedBy? subscriptionType,
-    @JsonKey(name: 'customer_type') ApprovedBy? customerType,
-    @JsonKey(name: 'company_location') CompanyLocation? companyLocation,
-    @JsonKey(name: 'owner_address') ApprovedBy? ownerAddress,
-    @JsonKey(name: 'owner_national_id') ApprovedBy? ownerNationalId,
-    @JsonKey(name: 'owner_tax_id') ApprovedBy? ownerTaxId,
-    @JsonKey(name: 'requested_by') ApprovedBy? requestedBy,
-    @JsonKey(name: 'owner_national_id_photo') ApprovedBy? ownerNationalIdPhoto,
-    @JsonKey(name: 'company_address') ApprovedBy? companyAddress,
-    @JsonKey(name: 'note') ApprovedBy? note,
-    @JsonKey(name: 'company_email') ApprovedBy? companyEmail,
-    @JsonKey(name: 'approved_by') ApprovedBy? approvedBy,
-    @JsonKey(name: 'ownership_status') ApprovedBy? ownershipStatus,
-    @JsonKey(name: 'company_store_photo') CompanyStorePhoto? companyStorePhoto,
-    @JsonKey(name: 'company_name') ApprovedBy? companyName,
-    @JsonKey(name: 'owner_phone_number') ApprovedBy? ownerPhoneNumber,
+    @JsonKey(name: 'pic_phone_number') ApprovedBy? picPhoneNumber,
     @JsonKey(name: 'blacklisted') Blacklisted? blacklisted,
+    @JsonKey(name: 'company_address') ApprovedBy? companyAddress,
+    @JsonKey(name: 'pic_address') ApprovedBy? picAddress,
+    @JsonKey(name: 'credit_limit') Credit? creditLimit,
+    @JsonKey(name: 'company_store_photo') ApprovedBy? companyStorePhoto,
+    @JsonKey(name: 'note') ApprovedBy? note,
+    @JsonKey(name: 'company_tax_id') ApprovedBy? companyTaxId,
+    @JsonKey(name: 'credit_period') Credit? creditPeriod,
+    @JsonKey(name: 'pic_position') ApprovedBy? picPosition,
+    @JsonKey(name: 'company_email') ApprovedBy? companyEmail,
+    @JsonKey(name: 'company_name') ApprovedBy? companyName,
     @JsonKey(name: 'company_phone_number') ApprovedBy? companyPhoneNumber,
+    @JsonKey(name: 'company_location') CompanyLocation? companyLocation,
+    @JsonKey(name: 'pic_tax_id') ApprovedBy? picTaxId,
+    @JsonKey(name: 'customer_type') ApprovedBy? customerType,
+    @JsonKey(name: 'subscription_type') ApprovedBy? subscriptionType,
+    @JsonKey(name: 'ownership_status') ApprovedBy? ownershipStatus,
+    @JsonKey(name: 'requested_by') ApprovedBy? requestedBy,
+    @JsonKey(name: 'pic_name') ApprovedBy? picName,
+    @JsonKey(name: 'approved_by') ApprovedBy? approvedBy,
+    @JsonKey(name: 'pic_national_id') ApprovedBy? picNationalId,
   }) = _Fields;
 
   factory Fields.fromJson(Map<String, dynamic> json) => _$FieldsFromJson(json);
@@ -87,8 +91,8 @@ class MapValue with _$MapValue {
 class MapValueFields with _$MapValueFields {
   const factory MapValueFields({
     @JsonKey(name: 'accuracy') Accuracy? accuracy,
-    @JsonKey(name: 'latitude') Itude? latitude,
-    @JsonKey(name: 'longitude') Itude? longitude,
+    @JsonKey(name: 'longitude') Accuracy? longitude,
+    @JsonKey(name: 'latitude') Accuracy? latitude,
   }) = _MapValueFields;
 
   factory MapValueFields.fromJson(Map<String, dynamic> json) =>
@@ -105,19 +109,9 @@ class Accuracy with _$Accuracy {
 }
 
 @freezed
-class Itude with _$Itude {
-  const factory Itude({@JsonKey(name: 'doubleValue') double? doubleValue}) =
-      _Itude;
+class Credit with _$Credit {
+  const factory Credit({@JsonKey(name: 'integerValue') String? integerValue}) =
+      _Credit;
 
-  factory Itude.fromJson(Map<String, dynamic> json) => _$ItudeFromJson(json);
-}
-
-@freezed
-class CompanyStorePhoto with _$CompanyStorePhoto {
-  const factory CompanyStorePhoto({
-    @JsonKey(name: 'stringValue') String? stringValue,
-  }) = _CompanyStorePhoto;
-
-  factory CompanyStorePhoto.fromJson(Map<String, dynamic> json) =>
-      _$CompanyStorePhotoFromJson(json);
+  factory Credit.fromJson(Map<String, dynamic> json) => _$CreditFromJson(json);
 }
