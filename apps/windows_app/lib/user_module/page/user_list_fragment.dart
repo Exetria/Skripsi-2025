b27b50b9ad2a84ef2995cc218df3d8b6e9bc67f1
Exetria <107202768+Exetria@users.noreply.github.com>
2025-06-21 22:02:10 +0700

    Windows - View Announcements Mode in User List

--- apps/windows_app/lib/user_module/page/user_list_fragment.dart
+++ apps/windows_app/lib/user_module/page/user_list_fragment.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:windows_app/user_module/domain/entities/announcement_domain.dart';
 import 'package:windows_app/user_module/domain/entities/user_domain.dart';
+import 'package:windows_app/user_module/page/controller/announcement_list_controller.dart';
 import 'package:windows_app/user_module/page/controller/user_list_controller.dart';
 import 'package:windows_app/utils/functions.dart';
 import 'package:windows_app/utils/popups.dart';
@@ -17,6 +19,7 @@ class UserListFragment extends StatefulHookConsumerWidget {
 
 class _UserListFragment extends ConsumerState<UserListFragment> {
   RoleFilter selectedRole = RoleFilter.all;
+  bool seeUser = true;
 
   @override
   void initState() {
@@ -31,6 +34,7 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
   @override
   Widget build(BuildContext context) {
     final userListState = ref.watch(userListControllerProvider);
+    final announcementListState = ref.watch(announcementListControllerProvider);
 
     return Padding(
       padding: const EdgeInsets.all(16),
@@ -46,77 +50,10 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
 
           // Content area
           Expanded(
-            child: userListState.when(
-              loading: () => const Center(child: CircularProgressIndicator()),
-              data: (salesList) {
-                if (salesList == null || salesList.isEmpty) {
-                  return Center(
-                    child: Text(
-                      'Data Pengguna Tidak Ditemukan',
-                      style: bodyStyle,
-                    ),
-                  );
-                }
-
-                return LayoutBuilder(
-                  builder: (context, constraints) {
-                    final crossCount = getCrossAxisCount(constraints);
-                    return GridView.builder(
-                      padding: const EdgeInsets.only(top: 8),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: crossCount,
-                        crossAxisSpacing: 16,
-                        mainAxisSpacing: 16,
-                        childAspectRatio: getChildAspectRatio(constraints),
-                      ),
-                      itemCount: salesList.length,
-                      itemBuilder: (context, index) {
-                        final data = salesList[index];
-
-                        final name = data.fields?.fullName?.stringValue ?? '-';
-                        final email = data.fields?.email?.stringValue ?? '-';
-                        final role = data.fields?.role?.stringValue ?? '-';
-                        final registerDate = DateFormat(
-                          'd MMM yyyy',
-                        ).format(DateTime.parse(data.createTime ?? ''));
-
-                        return itemCard(
-                          context: context,
-                          icon: Icons.person,
-                          title: name,
-                          subtitle: email,
-                          secondarySubtitle:
-                              data.fields?.phoneNumber?.stringValue != null
-                                  ? phoneNumberFormat(
-                                    data.fields?.phoneNumber?.stringValue ?? '',
-                                  )
-                                  : null,
-                          leftBottomText:
-                              role[0].toUpperCase() + role.substring(1),
-                          rightBottomText: registerDate.toString(),
-                          onTap: () {
-                            showUserDataPopup(
-                              ref: ref,
-                              context: context,
-                              userData: data,
-                            );
-                          },
-                        );
-                      },
-                    );
-                  },
-                );
-              },
-              error: (error, _) {
-                final exception = error as ApiException;
-                return Center(
-                  child: Text(
-                    'Gagal Memuat Daftar Pengguna: ${exception.message}',
-                    style: errorStyle,
-                  ),
-                );
-              },
-            ),
+            child:
+                seeUser
+                    ? _buildUserList(userListState)
+                    : _buildAnnouncementList(announcementListState),
           ),
         ],
       ),
@@ -132,8 +69,10 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
             SizedBox(
               width: ScreenUtil().screenWidth * 0.25,
               child: customSearchBar(
+                enabled: seeUser,
                 context: context,
                 hint: 'Cari Pengguna...',
+
                 onChanged: (query) {
                   ref
                       .read(userListControllerProvider.notifier)
@@ -145,6 +84,11 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
 
             IconButton(
               onPressed: () {
+                if (!seeUser) {
+                  setState(() {
+                    seeUser = true;
+                  });
+                }
                 selectedRole = RoleFilter.all;
                 ref
                     .read(userListControllerProvider.notifier)
@@ -153,7 +97,7 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
               icon: Icon(
                 Icons.group_work,
                 color:
-                    selectedRole == RoleFilter.all
+                    selectedRole == RoleFilter.all && seeUser
                         ? Theme.of(context).colorScheme.primary
                         : Theme.of(context).colorScheme.onSurface,
               ),
@@ -163,6 +107,11 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
 
             IconButton(
               onPressed: () {
+                if (!seeUser) {
+                  setState(() {
+                    seeUser = true;
+                  });
+                }
                 selectedRole = RoleFilter.admin;
                 ref
                     .read(userListControllerProvider.notifier)
@@ -171,7 +120,7 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
               icon: Icon(
                 Icons.admin_panel_settings,
                 color:
-                    selectedRole == RoleFilter.admin
+                    selectedRole == RoleFilter.admin && seeUser
                         ? Theme.of(context).colorScheme.primary
                         : Theme.of(context).colorScheme.onSurface,
               ),
@@ -181,6 +130,11 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
 
             IconButton(
               onPressed: () {
+                if (!seeUser) {
+                  setState(() {
+                    seeUser = true;
+                  });
+                }
                 selectedRole = RoleFilter.sales;
                 ref
                     .read(userListControllerProvider.notifier)
@@ -189,11 +143,27 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
               icon: Icon(
                 Icons.people,
                 color:
-                    selectedRole == RoleFilter.sales
+                    selectedRole == RoleFilter.sales && seeUser
                         ? Theme.of(context).colorScheme.primary
                         : Theme.of(context).colorScheme.onSurface,
               ),
               tooltip: 'Tampilkan Sales',
+            ),
+            const SizedBox(width: 12),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  seeUser = false;
+                });
+              },
+              icon: Icon(
+                Icons.announcement,
+                color:
+                    !seeUser
+                        ? Theme.of(context).colorScheme.primary
+                        : Theme.of(context).colorScheme.onSurface,
+              ),
+              tooltip: 'Tampilkan Daftar Pengumuman',
             ),
           ],
         ),
@@ -221,13 +191,141 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
             const SizedBox(width: 8),
 
             IconButton(
-              onPressed: _refreshUserList,
+              onPressed: seeUser ? _refreshUserList : _refreshAnnouncementList,
               icon: const Icon(Icons.refresh),
               tooltip: 'Segarkan',
             ),
           ],
         ),
       ],
+    );
+  }
+
+  Widget _buildUserList(AsyncValue<List<UserDomain>?> userListState) {
+    return userListState.when(
+      loading: () => const Center(child: CircularProgressIndicator()),
+      data: (salesList) {
+        if (salesList == null || salesList.isEmpty) {
+          return Center(
+            child: Text('Data Pengguna Tidak Ditemukan', style: bodyStyle),
+          );
+        }
+
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            final crossCount = getCrossAxisCount(constraints);
+            return GridView.builder(
+              padding: const EdgeInsets.only(top: 8),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossCount,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: getChildAspectRatio(constraints),
+              ),
+              itemCount: salesList.length,
+              itemBuilder: (context, index) {
+                final data = salesList[index];
+
+                final name = data.fields?.fullName?.stringValue ?? '-';
+                final email = data.fields?.email?.stringValue ?? '-';
+                final role = data.fields?.role?.stringValue ?? '-';
+                final registerDate = DateFormat(
+                  'd MMM yyyy',
+                ).format(DateTime.parse(data.createTime ?? ''));
+
+                return itemCard(
+                  context: context,
+                  icon: Icons.person,
+                  title: name,
+                  subtitle: email,
+                  secondarySubtitle:
+                      data.fields?.phoneNumber?.stringValue != null
+                          ? phoneNumberFormat(
+                            data.fields?.phoneNumber?.stringValue ?? '',
+                          )
+                          : null,
+                  leftBottomText: role[0].toUpperCase() + role.substring(1),
+                  rightBottomText: registerDate.toString(),
+                  onTap: () {
+                    showUserDataPopup(
+                      ref: ref,
+                      context: context,
+                      userData: data,
+                    );
+                  },
+                );
+              },
+            );
+          },
+        );
+      },
+      error: (error, _) {
+        final exception = error as ApiException;
+        return Center(
+          child: Text(
+            'Gagal Memuat Daftar Pengguna: ${exception.message}',
+            style: errorStyle,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildAnnouncementList(
+    AsyncValue<List<AnnouncementDomain>?> announcementListState,
+  ) {
+    return announcementListState.when(
+      loading: () => const Center(child: CircularProgressIndicator()),
+      data: (announcementList) {
+        if (announcementList == null || announcementList.isEmpty) {
+          return Center(
+            child: Text('Data Pengumuman Tidak Ditemukan', style: bodyStyle),
+          );
+        }
+
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            final crossCount = getCrossAxisCount(constraints);
+            return GridView.builder(
+              padding: const EdgeInsets.only(top: 8),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossCount,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: getChildAspectRatio(constraints),
+              ),
+              itemCount: announcementList.length,
+              itemBuilder: (context, index) {
+                final data = announcementList[index];
+
+                final title = data.fields?.title?.stringValue ?? '-';
+                final content = data.fields?.content?.stringValue ?? '-';
+                final creationDate = DateFormat(
+                  'd MMM yyyy',
+                ).format(DateTime.parse(data.createTime ?? ''));
+
+                return itemCard(
+                  context: context,
+                  icon: Icons.announcement,
+                  title: title,
+                  subtitle: content,
+                  rightBottomText: creationDate.toString(),
+                  onTap: () {},
+                );
+              },
+            );
+          },
+        );
+      },
+      error: (error, _) {
+        final exception = error as ApiException;
+        return Center(
+          child: Text(
+            'Gagal Memuat Daftar Pengumuman: ${exception.message}',
+            style: errorStyle,
+          ),
+        );
+      },
     );
   }
 
@@ -243,5 +341,9 @@ class _UserListFragment extends ConsumerState<UserListFragment> {
 
   Future<void> _refreshUserList() async {
     ref.invalidate(userListControllerProvider);
+  }
+
+  Future<void> _refreshAnnouncementList() async {
+    ref.invalidate(announcementListControllerProvider);
   }
 }
